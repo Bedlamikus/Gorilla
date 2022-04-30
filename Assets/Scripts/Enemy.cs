ec60e7494109272b814de1b4b489de8911c490d8
Bedlamikus <Bedlamikus@gmail.com>
2022-04-30 22:14:06 +0700

    Enemy AI

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _radiusView;
+
+    [SerializeField]
+    private int _enemySpeed;
+
+    private Player _player;
+    private NavMeshAgent _agent;
+
+    void Start()
+    {
+        _player = FindObjectOfType<Player>();
+        if (_player == null)
+        {
+            throw new System.Exception("Enemy not find gameObject Player!!!");
+        }
+        _agent = GetComponent<NavMeshAgent>();
+        if (_agent == null)
+        {
+            print(this.name + " can't find component NavMeshAgent!!");
+            return;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToPlayer();
+    }
+
+    private void MoveToPlayer()
+    {
+        if (_agent == null)
+        {
+            return;
+        }
+        MoovingEnemy(_player.transform.position);
+    }
+
+    private void MoovingEnemy(Vector3 direction)
+    {
+        if ((_player.transform.position - transform.position).magnitude >= _radiusView)
+        {
+            return;
+        }
+        transform.LookAt(direction);
+        _agent.SetDestination(_player.transform.position);
+        Quaternion _quaternion = transform.rotation;
+        _quaternion.x = 0;
+        _quaternion.z = 0;
+        transform.rotation = _quaternion;
+    }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputController : MonoBehaviour
+{
+    private Joystick _joyStick;
+    private bool _mooving;
+    private Vector2 _direction;
+    private Player _player;
+
+    public int _playerSpeed;
+
+    void Start()
+    {
+        _joyStick = FindObjectOfType<Joystick>();
+        if (_joyStick == null) throw new System.Exception("Joystik not find");
+        _mooving = false;
+        TryFindPlayer();
+    }
+
+    void Update()
+    {
+        if (_joyStick.Direction == Vector2.zero)
+        {
+            if (_mooving == true)
+            {
+                _mooving = false;
+                _direction = Vector2.zero;
+                MoovingPlayer(_direction);
+            }
+            return;
+        }
+        _mooving = true;
+        _direction = _joyStick.Direction;
+        MoovingPlayer(_direction);
+
+    }
+
+    private void MoovingPlayer(Vector2 direction)
+    {
+        if (_player == null)
+        {
+            TryFindPlayer();
+        }
+        Transform tr = _player.transform;
+        Vector3 position = tr.position;
+        if (direction.magnitude > 0.1)
+            tr.rotation = Quaternion.Euler(0, 180 * Mathf.Atan2(direction.y, -direction.x) / Mathf.PI, 0);
+
+        Vector3 desiredMove = tr.forward * direction.magnitude;
+
+        position.x += -desiredMove.z * _playerSpeed * Time.deltaTime;
+        position.z += desiredMove.x * _playerSpeed * Time.deltaTime;
+
+        _player.transform.SetPositionAndRotation(position, tr.rotation);
+
+    }
+
+    private void TryFindPlayer()
+    {
+        _player = FindObjectOfType<Player>();
+        if (_player == null) throw new System.Exception("Player not find");
+    }
+}
